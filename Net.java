cb4c7ad5dc8eb7a28d3cf53662010cdd47c068fa
Ramu Bhusal (Alex) <ramubhusal95@gmail.com>
2017-07-16 09:48:56 +0545

    Update Net.java

--- Net.java
+++ Net.java
@@ -4,21 +4,20 @@ import java.io.*;
 class Net{
 	public static void main(String args[]){
 		try{
-			URL u=new URL("http://ramubhusal.com.np");
-			InetAddress ip[]=InetAddress.getAllByName("www.google.com");
-			System.out.println(ip[1]);
-			//URLConnection uc=u.openConnection();
-			//uc.connect();
-			//String x=u.openStream();
-			//DataInputStream dis=new DataInputStream(u.openStream());
-			//FileOutputStream dos=new FileOutputStream("new.html");
-			//int i;
-			//while((i=dis.read())!=-1){
-				//System.out.print((char)i);
-			//	dos.write(i);
+			URL u=new URL("http://www.google.com.np"); //change website name here
+			InetAddress ip=InetAddress.getByName("www.google.com"); //change website name here
+			System.out.println(ip);
+			URLConnection uc=u.openConnection();
+			uc.connect();
+			DataInputStream dis=new DataInputStream(u.openStream());
+			FileOutputStream dos=new FileOutputStream("new.html"); 
+			int i;
+			while((i=dis.read())!=-1){
+				System.out.print((char)i);
+				dos.write(i);
 				
-			//}
-			//dos.close();
+			}
+			dos.close();
 			
 		}catch(Exception e){
 			System.out.println("Error"+e);
